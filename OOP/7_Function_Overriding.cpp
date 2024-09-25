4afdfb25c3fba491518461b575cb8836f982c17c
Zettex <safaandsafa4@gmail.com>
2024-09-25 15:23:54 +0600

    all the code files completed

--- OOP/7_Function_Overriding.cpp
+++ OOP/7_Function_Overriding.cpp
@@ -1,3 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Animal{
+    public:
+        virtual void sound(){
+            cout<<"I am a Random Animal!"<<endl;
+        }
+};
+
+class Bird : public Animal{
+    public:
+        void sound() override{
+            cout<<"I am a Bird.Chirp chip!"<<endl;
+        }
+};
+
+class Cow : public Animal{
+    public:
+        void sound() override{
+            cout<<"I am a cow.Moo!"<<endl;
+        }
+};
+
+// here every class inherited the animal class and manipulated the sound function.
+// This is an example of function overriding.
+
+int main(){
+    Animal* random = new Animal();
+    Animal* cow = new Cow();
+    Animal* bird = new Bird();
+
+    random->sound();
+    cow->sound();
+    bird->sound();
+}
 
 
--- OOP/7_Function_Overriding.py
+++ OOP/7_Function_Overriding.py
@@ -1,5 +1,23 @@
+class Animal:
+    def sound(self):
+        print("I am a random animal. :( ")
+
+class Bird(Animal):
+    def sound(self):
+        print("I am a Bird.Chirp chirp!")
+
+class Cow(Animal):
+    def sound(self):
+        print("I am a Cow.Moo!")
 
 
+random_animal = Animal()
+cow = Cow()
+bird = Bird()
+
+random_animal.sound()
+bird.sound()
+cow.sound()
 
 
 # Runtime Polymorphism -> Dynamic Binding | Overriding | Late Binding
